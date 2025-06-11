a0e4bc2b46bdb01f494641082875e102ed891904
YAP WEI JUN <yapweijun1996@gmail.com>
2025-06-11 16:11:14 +0800

    Add multi-page support

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
 export default {
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        page2: 'page2.html',
+        page3: 'page3.html',
+        page4: 'page4.html',
+        page5: 'page5.html'
+      }
+    }
+  }
 }
